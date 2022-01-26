3699e1b394f3a74c09319c038afcde20d6412c21
officalju <e244608@edu.sbl.ch>
2022-01-26 08:52:14 +0000

    Bio

--- run.py
+++ run.py
@@ -28,11 +28,6 @@ def home():
 @app.route("/question")
 def question():
     return render_template("question.html", question=random_question())
-<<<<<<< HEAD
-=======
-
->>>>>>> 2a030b35c74218067bac83fb978de3d612503fc7
-
 
 @app.route("/answer/<int:question_id>/<int:answer_id>")
 def answer(question_id, answer_id):
