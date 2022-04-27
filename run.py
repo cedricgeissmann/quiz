70cbc29feb2451033ec58e76f98c802a4ccc3262
Lilia2828 <e227809@sbl.ch>
2022-04-27 12:16:36 +0000

    changes

--- run.py
+++ run.py
@@ -1,41 +1,22 @@
 #! /usr/bin/env python
 from flask import Flask, render_template
-import json
-from random import choice
+
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 
-def random_question():
-    with open("questions.json", 'r') as f:
-        questions = json.load(f)
-    return choice(questions)
-
-def check_answer(q_id, a_id):
-    with open("questions.json", 'r') as f:
-        questions = json.load(f)
-    q = list(filter(lambda x: x["id"] == q_id, questions))[0]
-    return q["correct"] == a_id
-
-
-
-@app.route("/")
+@app.route("/index")
 def home():
     return render_template("index.html")
 
-@app.route("/question")
-def question():
-    return render_template("question.html", question=random_question())
+@app.route("/spielanleitung")
+def spielanleitung():
+    return render_template("spielanleitung.html")
 
-
-@app.route("/answer/<int:question_id>/<int:answer_id>")
-def answer(question_id, answer_id):
-    correct = check_answer(question_id, answer_id)
-    return render_template("answer.html", correct=correct)
 
 @app.route("/irem")
-def numa():
+def irem():
     return render_template("Irem.html")
 
 @app.route("/olivia")
