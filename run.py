699d0c588cd710472e8dbb8ae2ef25b737202775
Sa1om3 <e244548@sbl.ch>
2022-04-27 08:26:05 +0000

    created game website

--- run.py
+++ run.py
@@ -7,16 +7,6 @@ app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 
-def random_question():
-    with open("questions.json", 'r') as f:
-        questions = json.load(f)
-    return choice(questions)
-
-def check_answer(q_id, a_id):
-    with open("questions.json", 'r') as f:
-        questions = json.load(f)
-    q = list(filter(lambda x: x["id"] == q_id, questions))[0]
-    return q["correct"] == a_id
 
 
 
@@ -25,15 +15,9 @@ def home():
     return render_template("index.html")
 
 
-@app.route("/question")
-def question():
-    return render_template("question.html", question=random_question())
 
 
-@app.route("/answer/<int:question_id>/<int:answer_id>")
-def answer(question_id, answer_id):
-    correct = check_answer(question_id, answer_id)
-    return render_template("answer.html", correct=correct)
+
 
 @app.route("/salome")
 def salome():
@@ -43,9 +27,6 @@ def salome():
 def anja():
     return render_template("Anja.html")
 
-@app.route("/us")
-def us():
-    return render_template("Us.html")
 
 
 
